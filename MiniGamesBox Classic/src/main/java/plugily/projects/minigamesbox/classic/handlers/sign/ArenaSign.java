62e46e897d4b81946deb9a0a4539e004de355cb7
Tigerpanzer_02 <legionofunicorns0202@gmail.com>
2021-10-07 20:53:01 +0200

    Added sign function

---
+++ MiniGamesBox Classic/src/main/java/plugily/projects/minigamesbox/classic/handlers/sign/ArenaSign.java
@@ -0,0 +1,92 @@
+/*
+ * MiniGamesBox - Library box with massive content that could be seen as minigames core.
+ * Copyright (C)  2021  Plugily Projects - maintained by Tigerpanzer_02 and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package plugily.projects.minigamesbox.classic.handlers.sign;
+
+import org.bukkit.Location;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
+import org.bukkit.block.Sign;
+import org.jetbrains.annotations.Nullable;
+import plugily.projects.minigamesbox.classic.utils.helper.MaterialUtils;
+import plugily.projects.minigamesbox.classic.utils.version.ServerVersion;
+
+import java.lang.reflect.InvocationTargetException;
+/**
+ * @author Tigerpanzer_02
+ * <p>
+ * Created at 21.09.2021
+ */
+
+/**
+ * Created for 1.14 compatibility purposes, it will cache block behind sign that will be
+ * accessed via reflection on 1.14 which is expensive
+ */
+public class ArenaSign {
+
+  private final Sign sign;
+  private Block behind;
+  private final Arena arena;
+
+  public ArenaSign(Sign sign, Arena arena) {
+    this.sign = sign;
+    this.arena = arena;
+    setBehindBlock();
+  }
+
+  private void setBehindBlock() {
+    behind = null;
+    if(MaterialUtils.isWallSign(sign.getBlock().getType())) {
+      behind = ServerVersion.Version.isCurrentEqualOrHigher(ServerVersion.Version.v1_14_R1) ? getBlockBehind() : getBlockBehindLegacy();
+    }
+  }
+
+  private Block getBlockBehind() {
+    try {
+      org.bukkit.block.BlockState state = sign.getBlock().getState();
+      Object blockData = state.getClass().getMethod("getBlockData").invoke(state);
+      BlockFace face = (BlockFace) blockData.getClass().getMethod("getFacing").invoke(blockData);
+
+      Location loc = sign.getLocation();
+      return new Location(loc.getWorld(), loc.getBlockX() - face.getModX(), loc.getBlockY() - face.getModY(),
+          loc.getBlockZ() - face.getModZ()).getBlock();
+    } catch(NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+      e.printStackTrace();
+      return null;
+    }
+  }
+
+  private Block getBlockBehindLegacy() {
+    return sign.getBlock().getRelative(((org.bukkit.material.Sign) sign.getData()).getAttachedFace());
+  }
+
+  public Sign getSign() {
+    return sign;
+  }
+
+  @Nullable
+  public Block getBehind() {
+    return behind;
+  }
+
+  public Arena getArena() {
+    return arena;
+  }
+
+}
---
+++ MiniGamesBox Classic/src/main/java/plugily/projects/minigamesbox/classic/handlers/sign/SignManager.java
@@ -0,0 +1,257 @@
+/*
+ * MiniGamesBox - Library box with massive content that could be seen as minigames core.
+ * Copyright (C)  2021  Plugily Projects - maintained by Tigerpanzer_02 and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package plugily.projects.minigamesbox.classic.handlers.sign;
+
+import org.apache.commons.lang.StringUtils;
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.block.Block;
+import org.bukkit.block.Sign;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.SignChangeEvent;
+import org.jetbrains.annotations.Nullable;
+import plugily.projects.minigamesbox.classic.Main;
+import plugily.projects.minigamesbox.classic.utils.configuration.ConfigUtils;
+import plugily.projects.minigamesbox.classic.utils.misc.complement.ComplementAccessor;
+import plugily.projects.minigamesbox.classic.utils.serialization.LocationSerializer;
+import plugily.projects.minigamesbox.classic.utils.version.ServerVersion;
+import plugily.projects.minigamesbox.classic.utils.version.events.api.CBPlayerInteractEvent;
+import plugily.projects.minigamesbox.inventory.util.XMaterial;
+
+import java.util.ArrayList;
+import java.util.EnumMap;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Level;
+/**
+ * @author Tigerpanzer_02
+ * <p>
+ * Created at 21.09.2021
+ */
+public class SignManager implements Listener {
+
+  private final Main plugin;
+  private final List<ArenaSign> arenaSigns = new ArrayList<>();
+  private final Map<ArenaState, String> gameStateToString = new EnumMap<>(ArenaState.class);
+  private final List<String> signLines;
+
+  public SignManager(Main plugin) {
+    this.plugin = plugin;
+    gameStateToString.put(ArenaState.WAITING_FOR_PLAYERS, plugin.getChatManager().colorMessage(Messages.SIGNS_GAME_STATES_INACTIVE));
+    gameStateToString.put(ArenaState.STARTING, plugin.getChatManager().colorMessage(Messages.SIGNS_GAME_STATES_STARTING));
+    gameStateToString.put(ArenaState.IN_GAME, plugin.getChatManager().colorMessage(Messages.SIGNS_GAME_STATES_IN_GAME));
+    gameStateToString.put(ArenaState.ENDING, plugin.getChatManager().colorMessage(Messages.SIGNS_GAME_STATES_ENDING));
+    gameStateToString.put(ArenaState.RESTARTING, plugin.getChatManager().colorMessage(Messages.SIGNS_GAME_STATES_RESTARTING));
+    signLines = LanguageManager.getLanguageList("Signs.Lines");
+    plugin.getServer().getPluginManager().registerEvents(this, plugin);
+  }
+
+  @EventHandler
+  public void onSignChange(SignChangeEvent e) {
+    if(!e.getPlayer().hasPermission("villagedefense.admin.sign.create")
+        || !ComplementAccessor.getComplement().getLine(e, 0).equalsIgnoreCase("[villagedefense]")) {
+      return;
+    }
+    String line1 = ComplementAccessor.getComplement().getLine(e, 1);
+    if(line1.isEmpty()) {
+      e.getPlayer().sendMessage(plugin.getChatManager().getPrefix() + plugin.getChatManager().colorMessage(Messages.COMMANDS_TYPE_ARENA_NAME));
+      return;
+    }
+    for(Arena arena : ArenaRegistry.getArenas()) {
+      if(!arena.getId().equalsIgnoreCase(line1)) {
+        continue;
+      }
+      for(int i = 0; i < signLines.size(); i++) {
+        ComplementAccessor.getComplement().setLine(e, i, formatSign(signLines.get(i), arena));
+      }
+      arenaSigns.add(new ArenaSign((Sign) e.getBlock().getState(), arena));
+      e.getPlayer().sendMessage(plugin.getChatManager().getPrefix() + plugin.getChatManager().colorMessage(Messages.SIGNS_SIGN_CREATED));
+      String location = e.getBlock().getWorld().getName() + "," + e.getBlock().getX() + "," + e.getBlock().getY() + "," + e.getBlock().getZ() + ",0.0,0.0";
+      FileConfiguration config = ConfigUtils.getConfig(plugin, "arenas");
+      List<String> locs = config.getStringList("instances." + arena.getId() + ".signs");
+      locs.add(location);
+      config.set("instances." + arena.getId() + ".signs", locs);
+      ConfigUtils.saveConfig(plugin, config, Constants.Files.ARENAS.getName());
+      return;
+    }
+    e.getPlayer().sendMessage(plugin.getChatManager().getPrefix() + plugin.getChatManager().colorMessage(Messages.SIGNS_ARENA_DOESNT_EXISTS));
+  }
+
+  private String formatSign(String msg, Arena a) {
+    String formatted = msg;
+    formatted = StringUtils.replace(formatted, "%mapname%", a.getMapName());
+    int maximumPlayers = a.getMaximumPlayers();
+    if(a.getPlayers().size() >= maximumPlayers) {
+      formatted = StringUtils.replace(formatted, "%state%", plugin.getChatManager().colorMessage(Messages.SIGNS_GAME_STATES_FULL_GAME));
+    } else {
+      formatted = StringUtils.replace(formatted, "%state%", gameStateToString.get(a.getArenaState()));
+    }
+    formatted = StringUtils.replace(formatted, "%playersize%", Integer.toString(a.getPlayers().size()));
+    formatted = StringUtils.replace(formatted, "%maxplayers%", Integer.toString(maximumPlayers));
+    formatted = plugin.getChatManager().colorRawMessage(formatted);
+    return formatted;
+  }
+
+  @EventHandler
+  public void onSignDestroy(BlockBreakEvent e) {
+    ArenaSign arenaSign = getArenaSignByBlock(e.getBlock());
+    if(!e.getPlayer().hasPermission("villagedefense.admin.sign.break") || arenaSign == null) {
+      return;
+    }
+    arenaSigns.remove(arenaSign);
+    FileConfiguration config = ConfigUtils.getConfig(plugin, "arenas");
+    if(!config.isConfigurationSection("instances")) {
+      return;
+    }
+
+    String location = e.getBlock().getWorld().getName() + "," + e.getBlock().getX() + "," + e.getBlock().getY() + "," + e.getBlock().getZ() + "," + "0.0,0.0";
+    for(String arena : config.getConfigurationSection("instances").getKeys(false)) {
+      for(String sign : config.getStringList("instances." + arena + ".signs")) {
+        if(!sign.equals(location)) {
+          continue;
+        }
+        List<String> signs = config.getStringList("instances." + arena + ".signs");
+        signs.remove(location);
+        config.set("instances." + arena + ".signs", signs);
+        ConfigUtils.saveConfig(plugin, config, "arenas");
+        e.getPlayer().sendMessage(plugin.getChatManager().getPrefix() + plugin.getChatManager().colorMessage(Messages.SIGNS_SIGN_REMOVED));
+        return;
+      }
+    }
+    e.getPlayer().sendMessage(plugin.getChatManager().getPrefix() + ChatColor.RED + "Couldn't remove sign from configuration! Please do this manually!");
+  }
+
+  @EventHandler(priority = EventPriority.HIGH)
+  public void onJoinAttempt(CBPlayerInteractEvent e) {
+    ArenaSign arenaSign = getArenaSignByBlock(e.getClickedBlock());
+    if(e.getAction() == Action.RIGHT_CLICK_BLOCK && e.getClickedBlock().getState() instanceof Sign && arenaSign != null) {
+      Arena arena = arenaSign.getArena();
+      if(arena != null) {
+        ArenaManager.joinAttempt(e.getPlayer(), arena);
+      }
+    }
+  }
+
+  @Nullable
+  private ArenaSign getArenaSignByBlock(Block block) {
+    if(block == null) {
+      return null;
+    }
+
+    for(ArenaSign sign : arenaSigns) {
+      if(sign.getSign().getLocation().equals(block.getLocation())) {
+        return sign;
+      }
+    }
+
+    return null;
+  }
+
+  public void loadSigns() {
+    plugin.getDebugger().debug("Signs load event started");
+    long start = System.currentTimeMillis();
+
+    arenaSigns.clear();
+    FileConfiguration config = ConfigUtils.getConfig(plugin, "arenas");
+    org.bukkit.configuration.ConfigurationSection section = config.getConfigurationSection("instances");
+    if(section == null) {
+      plugin.getDebugger().debug(Level.WARNING, "No arena instances found. Signs won't be loaded");
+      return;
+    }
+
+    for(String path : section.getKeys(false)) {
+      for(String sign : section.getStringList(path + ".signs")) {
+        Location loc = LocationSerializer.getLocation(sign);
+        if(loc.getBlock().getState() instanceof Sign) {
+          arenaSigns.add(new ArenaSign((Sign) loc.getBlock().getState(), ArenaRegistry.getArena(path)));
+          continue;
+        }
+        plugin.getDebugger().debug(Level.WARNING, "Block at location {0} for arena {1} is not a sign!", LocationSerializer.locationToString(loc), path);
+      }
+    }
+    plugin.getDebugger().debug("Sign load event finished took {0}ms", System.currentTimeMillis() - start);
+  }
+
+  public void updateSigns() {
+    plugin.getDebugger().performance("SignUpdate", "[PerformanceMonitor] [SignUpdate] Updating signs");
+    long start = System.currentTimeMillis();
+
+    for(ArenaSign arenaSign : arenaSigns) {
+      for(int i = 0; i < signLines.size(); i++) {
+        ComplementAccessor.getComplement().setLine(arenaSign.getSign(), i, formatSign(signLines.get(i), arenaSign.getArena()));
+      }
+      if(plugin.getConfig().getBoolean("Signs-Block-States-Enabled", true) && arenaSign.getBehind() != null) {
+        Block behind = arenaSign.getBehind();
+        try {
+          switch(arenaSign.getArena().getArenaState()) {
+            case WAITING_FOR_PLAYERS:
+              behind.setType(XMaterial.WHITE_STAINED_GLASS.parseMaterial());
+              if(ServerVersion.Version.isCurrentLower(ServerVersion.Version.v1_13_R1)) {
+                Block.class.getMethod("setData", byte.class).invoke(behind, (byte) 0);
+              }
+              break;
+            case STARTING:
+              behind.setType(XMaterial.YELLOW_STAINED_GLASS.parseMaterial());
+              if(ServerVersion.Version.isCurrentLower(ServerVersion.Version.v1_13_R1)) {
+                Block.class.getMethod("setData", byte.class).invoke(behind, (byte) 4);
+              }
+              break;
+            case IN_GAME:
+              behind.setType(XMaterial.ORANGE_STAINED_GLASS.parseMaterial());
+              if(ServerVersion.Version.isCurrentLower(ServerVersion.Version.v1_13_R1)) {
+                Block.class.getMethod("setData", byte.class).invoke(behind, (byte) 1);
+              }
+              break;
+            case ENDING:
+              behind.setType(XMaterial.GRAY_STAINED_GLASS.parseMaterial());
+              if(ServerVersion.Version.isCurrentLower(ServerVersion.Version.v1_13_R1)) {
+                Block.class.getMethod("setData", byte.class).invoke(behind, (byte) 7);
+              }
+              break;
+            case RESTARTING:
+              behind.setType(XMaterial.BLACK_STAINED_GLASS.parseMaterial());
+              if(ServerVersion.Version.isCurrentLower(ServerVersion.Version.v1_13_R1)) {
+                Block.class.getMethod("setData", byte.class).invoke(behind, (byte) 15);
+              }
+              break;
+            default:
+              break;
+          }
+        } catch(Exception ignored) {
+        }
+      }
+      arenaSign.getSign().update();
+    }
+    plugin.getDebugger().performance("SignUpdate", "[PerformanceMonitor] [SignUpdate] Updated signs took {0}ms", System.currentTimeMillis() - start);
+  }
+
+  public List<ArenaSign> getArenaSigns() {
+    return arenaSigns;
+  }
+
+  public Map<ArenaState, String> getGameStateToString() {
+    return gameStateToString;
+  }
+}
