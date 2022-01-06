bf5df0833a891b2992a5c978f1e404beced73ac4
Tigerpanzer_02 <legionofunicorns0202@gmail.com>
2022-01-06 15:39:14 +0100

    (inventory) Added method to additems to itemmap

--- MiniGamesBox Inventory/src/main/java/plugily/projects/minigamesbox/inventory/common/item/ItemMap.java
+++ MiniGamesBox Inventory/src/main/java/plugily/projects/minigamesbox/inventory/common/item/ItemMap.java
@@ -10,105 +10,121 @@ import java.util.Map;
  * @author HSGamer
  */
 public class ItemMap {
-    /**
-     * The empty item map
-     */
-    public static final ItemMap EMPTY = new ItemMap(Collections.emptyMap());
+  /**
+   * The empty item map
+   */
+  public static final ItemMap EMPTY = new ItemMap(Collections.emptyMap());
 
-    private final Map<Integer, ClickableItem> items;
-    private ClickableItem defaultItem = null;
+  private final Map<Integer, ClickableItem> items;
+  private ClickableItem defaultItem = null;
 
-    /**
-     * Create an empty slot map
-     */
-    public ItemMap() {
-        this.items = new HashMap<>();
+  /**
+   * Create an empty slot map
+   */
+  public ItemMap() {
+    this.items = new HashMap<>();
+  }
+
+  /**
+   * Create a new slot map
+   *
+   * @param items the items
+   */
+  public ItemMap(Map<Integer, ClickableItem> items) {
+    this.items = items;
+  }
+
+  /**
+   * Create a new slot map
+   *
+   * @param items       the items
+   * @param defaultItem the default item
+   */
+  public ItemMap(Map<Integer, ClickableItem> items, ClickableItem defaultItem) {
+    this.items = items;
+    this.defaultItem = defaultItem;
+  }
+
+  /**
+   * Clone the slot map
+   *
+   * @param itemMap the slot map
+   */
+  public ItemMap(ItemMap itemMap) {
+    this.items = new HashMap<>(itemMap.getItems());
+    this.defaultItem = itemMap.getDefaultItem();
+  }
+
+  /**
+   * Set the item for the given slot
+   *
+   * @param slot the slot
+   * @param item the item
+   */
+  public void setItem(int slot, ClickableItem item) {
+    items.put(slot, item);
+  }
+
+
+  /**
+   * Add the item for next free slot
+   *
+   * @param item the item
+   */
+  public void addItem(ClickableItem item) {
+    for(int i = 0; i < 54; i++) {
+      if(!items.containsKey(i)) {
+        items.put(i, item);
+        return;
+      }
     }
+  }
 
-    /**
-     * Create a new slot map
-     *
-     * @param items the items
-     */
-    public ItemMap(Map<Integer, ClickableItem> items) {
-        this.items = items;
-    }
 
-    /**
-     * Create a new slot map
-     *
-     * @param items       the items
-     * @param defaultItem the default item
-     */
-    public ItemMap(Map<Integer, ClickableItem> items, ClickableItem defaultItem) {
-        this.items = items;
-        this.defaultItem = defaultItem;
-    }
+  /**
+   * Get the item for the given slot
+   *
+   * @param slot the slot
+   * @return the item
+   */
+  public ClickableItem getItem(int slot) {
+    return items.get(slot);
+  }
 
-    /**
-     * Clone the slot map
-     *
-     * @param itemMap the slot map
-     */
-    public ItemMap(ItemMap itemMap) {
-        this.items = new HashMap<>(itemMap.getItems());
-        this.defaultItem = itemMap.getDefaultItem();
-    }
+  /**
+   * Get the item for the given slot, if the slot is not set, return the default item
+   *
+   * @param slot the slot
+   * @return the item
+   */
+  public ClickableItem getItemOrDefault(int slot) {
+    return items.getOrDefault(slot, defaultItem);
+  }
 
-    /**
-     * Set the item for the given slot
-     *
-     * @param slot the slot
-     * @param item the item
-     */
-    public void setItem(int slot, ClickableItem item) {
-        items.put(slot, item);
-    }
+  /**
+   * Get the slot map
+   *
+   * @return the slot map
+   */
+  public Map<Integer, ClickableItem> getItems() {
+    return items;
+  }
 
-    /**
-     * Get the item for the given slot
-     *
-     * @param slot the slot
-     * @return the item
-     */
-    public ClickableItem getItem(int slot) {
-        return items.get(slot);
-    }
+  /**
+   * Get the default item
+   *
+   * @return the default item
+   */
+  public ClickableItem getDefaultItem() {
+    return defaultItem;
+  }
 
-    /**
-     * Get the item for the given slot, if the slot is not set, return the default item
-     *
-     * @param slot the slot
-     * @return the item
-     */
-    public ClickableItem getItemOrDefault(int slot) {
-        return items.getOrDefault(slot, defaultItem);
-    }
-
-    /**
-     * Get the slot map
-     *
-     * @return the slot map
-     */
-    public Map<Integer, ClickableItem> getItems() {
-        return items;
-    }
-
-    /**
-     * Get the default item
-     *
-     * @return the default item
-     */
-    public ClickableItem getDefaultItem() {
-        return defaultItem;
-    }
-
-    /**
-     * Set the default item
-     *
-     * @param defaultItem the default item
-     */
-    public void setDefaultItem(ClickableItem defaultItem) {
-        this.defaultItem = defaultItem;
-    }
+  /**
+   * Set the default item
+   *
+   * @param defaultItem the default item
+   */
+  public void setDefaultItem(ClickableItem defaultItem) {
+    this.defaultItem = defaultItem;
+  }
 }
--- MiniGamesBox Inventory/src/main/java/plugily/projects/minigamesbox/inventory/normal/NormalFastInv.java
+++ MiniGamesBox Inventory/src/main/java/plugily/projects/minigamesbox/inventory/normal/NormalFastInv.java
@@ -12,55 +12,64 @@ import plugily.projects.minigamesbox.inventory.common.item.ItemMap;
  * @author HSGamer
  */
 public class NormalFastInv extends RefreshableFastInv {
-    private final ItemMap itemMap = new ItemMap();
+  private final ItemMap itemMap = new ItemMap();
 
-    public NormalFastInv(int size) {
-        super(size);
-    }
+  public NormalFastInv(int size) {
+    super(size);
+  }
 
-    public NormalFastInv(int size, String title) {
-        super(size, title);
-    }
+  public NormalFastInv(int size, String title) {
+    super(size, title);
+  }
 
-    public NormalFastInv(InventoryType type) {
-        super(type);
-    }
+  public NormalFastInv(InventoryType type) {
+    super(type);
+  }
 
-    public NormalFastInv(InventoryType type, String title) {
-        super(type, title);
-    }
+  public NormalFastInv(InventoryType type, String title) {
+    super(type, title);
+  }
 
-    @Override
-    public ItemMap getItemMap() {
-        return itemMap;
-    }
+  @Override
+  public ItemMap getItemMap() {
+    return itemMap;
+  }
 
-    /**
-     * Sets the item in the specified slot.
-     *
-     * @param slot the slot to set the item in
-     * @param item the item to set
-     */
-    public void setItem(int slot, ClickableItem item) {
-        itemMap.setItem(slot, item);
-    }
+  /**
+   * Sets the item in the specified slot.
+   *
+   * @param slot the slot to set the item in
+   * @param item the item to set
+   */
+  public void setItem(int slot, ClickableItem item) {
+    itemMap.setItem(slot, item);
+  }
 
-    /**
-     * Sets the default item.
-     *
-     * @param item the item to set
-     */
-    public void setDefaultItem(ClickableItem item) {
-        itemMap.setDefaultItem(item);
-    }
+  /**
+   * Sets the item in the next free slot.
+   *
+   * @param item the item to set
+   */
+  public void addItem(ClickableItem item) {
+    itemMap.addItem(item);
+  }
 
-    /**
-     * Gets the item in the specified slot.
-     *
-     * @param slot the slot to get the item from
-     * @return the item in the specified slot
-     */
-    public ClickableItem getItem(int slot) {
-        return itemMap.getItem(slot);
-    }
+  /**
+   * Sets the default item.
+   *
+   * @param item the item to set
+   */
+  public void setDefaultItem(ClickableItem item) {
+    itemMap.setDefaultItem(item);
+  }
+
+  /**
+   * Gets the item in the specified slot.
+   *
+   * @param slot the slot to get the item from
+   * @return the item in the specified slot
+   */
+  public ClickableItem getItem(int slot) {
+    return itemMap.getItem(slot);
+  }
 }
