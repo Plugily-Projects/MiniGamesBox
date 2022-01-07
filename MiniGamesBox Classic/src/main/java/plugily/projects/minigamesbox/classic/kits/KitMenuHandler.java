8a435384aa5a4432549ea76f589ce0438de2caa3
Tigerpanzer_02 <legionofunicorns0202@gmail.com>
2022-01-07 12:05:47 +0100

    Fixed choose message of KitMenuHandler

--- MiniGamesBox Classic/src/main/java/plugily/projects/minigamesbox/classic/kits/KitMenuHandler.java
+++ MiniGamesBox Classic/src/main/java/plugily/projects/minigamesbox/classic/kits/KitMenuHandler.java
@@ -76,11 +76,11 @@ public class KitMenuHandler implements Listener {
           return;
         }
         if(!kit.isUnlockedByPlayer(player)) {
-          player.sendMessage(plugin.getChatManager().colorMessage("KIT_NOT_UNLOCKED").replace("%kit%", kit.getName()));
+          player.sendMessage(plugin.getChatManager().colorMessage("KIT_NOT_UNLOCKED").replace("%value%", kit.getName()));
           return;
         }
         plugin.getUserManager().getUser(player).setKit(kit);
-        player.sendMessage(plugin.getChatManager().colorMessage("KIT_CHOOSE").replace("%kit%", kit.getName()));
+        player.sendMessage(plugin.getChatManager().colorMessage("KIT_CHOOSE").replace("%value%", kit.getName()));
       });
     }
     gui.open(player);
