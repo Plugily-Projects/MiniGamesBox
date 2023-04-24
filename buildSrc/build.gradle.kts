2b89dc22e91c20df29f728384ae2a6a656a47278
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2023-04-24 09:59:49 -0400

    Forgot one

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     // Support convention plugins written in Groovy. Convention plugins are build scripts in 'src/main' that automatically become available as plugins in the main build.
-    id 'groovy-gradle-plugin'
+    id("groovy-gradle-plugin")
 }
 
 repositories {
