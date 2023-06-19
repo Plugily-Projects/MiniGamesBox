74c6c769e0a123693a37e9a6ec5b1f5527bfb145
Tigerpanzer_02 <37453987+Tigerpanzer02@users.noreply.github.com>
2023-06-19 21:33:41 +0200

    Fixed IndexOutOfBoundsException on PlaceholderCheckArgument

--- MiniGamesBox Classic/src/main/java/plugily/projects/minigamesbox/classic/commands/arguments/admin/PlaceholderCheckArgument.java
+++ MiniGamesBox Classic/src/main/java/plugily/projects/minigamesbox/classic/commands/arguments/admin/PlaceholderCheckArgument.java
@@ -22,6 +22,7 @@ import net.md_5.bungee.api.chat.ClickEvent;
 import net.md_5.bungee.api.chat.HoverEvent;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import plugily.projects.minigamesbox.classic.arena.PluginArena;
 import plugily.projects.minigamesbox.classic.commands.arguments.PluginArgumentsRegistry;
 import plugily.projects.minigamesbox.classic.commands.arguments.data.CommandArgument;
 import plugily.projects.minigamesbox.classic.commands.arguments.data.LabelData;
@@ -45,10 +46,19 @@ public class PlaceholderCheckArgument {
           return;
         }
         new MessageBuilder("---- PLACEHOLDERS OF " + registry.getPlugin().getPluginNamePrefixLong().toUpperCase() + " ----").send(sender);
+        PluginArena arena = registry.getPlugin().getArenaRegistry().getArena(((Player) sender));
+        if (arena == null) {
+          if (registry.getPlugin().getArenaRegistry().getArenas().isEmpty()) {
+            new MessageBuilder("!You need to have at least one arena!").send(sender);
+            return;
+          } else {
+            arena = registry.getPlugin().getArenaRegistry().getArenas().get(0);
+          }
+        }
         if(args.length == 2) {
           new MessageBuilder("---- INTERNAL PLACEHOLDERS ----").send(sender);
           for(Placeholder placeholder : registry.getPlugin().getPlaceholderManager().getRegisteredInternalPlaceholders()) {
-            String listMessage = new MessageBuilder("&aID #" + placeholder.getId() + "# &bTYPE " + placeholder.getPlaceholderType() + " &cEXECUTOR " + placeholder.getPlaceholderExecutor() + "%" + placeholder.getId() + "%").player((Player) sender).arena(registry.getPlugin().getArenaRegistry().getArenas().get(0)).build();
+            String listMessage = new MessageBuilder("&aID #" + placeholder.getId() + "# &bTYPE " + placeholder.getPlaceholderType() + " &cEXECUTOR " + placeholder.getPlaceholderExecutor() + "%" + placeholder.getId() + "%").player((Player) sender).arena(arena).build();
             new TextComponentBuilder(listMessage).player((Player) sender)
                 .setClickEvent(TextComponentBuilder.ClickAction.COPY_TO_CLIPBOARD, "%" + placeholder.getId() + "%")
                 .setHoverEvent(TextComponentBuilder.HoverAction.SHOW_TEXT, "%" + placeholder.getId() + "%")
@@ -59,7 +69,7 @@ public class PlaceholderCheckArgument {
           new MessageBuilder("---- EXTERNAL (PAPI) PLACEHOLDERS ----").send(sender);
           new MessageBuilder("NOTE: Parsing for specific arenas works with _arenaname:arena").send(sender);
           for(Placeholder placeholder : registry.getPlugin().getPlaceholderManager().getRegisteredPAPIPlaceholders()) {
-            String listMessage = new MessageBuilder("&aID #" + registry.getPlugin().getPluginNamePrefixLong() + "_" + placeholder.getId() + "# &bTYPE " + placeholder.getPlaceholderType() + " &cEXECUTOR " + placeholder.getPlaceholderExecutor() + "%" + registry.getPlugin().getPluginNamePrefixLong() + "_" + placeholder.getId() + "%").player((Player) sender).arena(registry.getPlugin().getArenaRegistry().getArenas().get(0)).build();
+            String listMessage = new MessageBuilder("&aID #" + registry.getPlugin().getPluginNamePrefixLong() + "_" + placeholder.getId() + "# &bTYPE " + placeholder.getPlaceholderType() + " &cEXECUTOR " + placeholder.getPlaceholderExecutor() + "%" + registry.getPlugin().getPluginNamePrefixLong() + "_" + placeholder.getId() + "%").player((Player) sender).arena(arena).build();
             new TextComponentBuilder(listMessage).player((Player) sender)
                 .setClickEvent(TextComponentBuilder.ClickAction.COPY_TO_CLIPBOARD, "%" + registry.getPlugin().getPluginNamePrefixLong() + "_" + placeholder.getId() + "%")
                 .setHoverEvent(TextComponentBuilder.HoverAction.SHOW_TEXT, "%" + registry.getPlugin().getPluginNamePrefixLong() + "_" + placeholder.getId() + "%")
