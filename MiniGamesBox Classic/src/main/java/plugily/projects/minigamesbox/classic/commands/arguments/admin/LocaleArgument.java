e646a5f23795cf936984958c900a8a885350cd4c
Wanbin Cao <93872019+Lagggpixel@users.noreply.github.com>
2024-01-02 23:34:27 +0000

    /xx locale <locale> which let you change the locale ingame (from locale storage) (#36)
    * fix: changed spelling mistakes
    
    Took 5 minutes
    
    * feat: changed access modifiers for LanguageManager.setupLocale from private to public
    
    Took 1 minute
    
    * feat: added Locale.getLocale, for usage see docs
    
    Took 35 seconds
    
    * feat: cleaned up "UTF-8" to use StandardCharsets.UTF_8 instead
    
    Took 48 seconds
    
    * feat: added LocaleArgument.java
    
    Took 36 seconds
    
    * feat: registering LocaleArgument.java in PluginArgumentsRegistry.java
    
    Took 33 seconds

---
+++ MiniGamesBox Classic/src/main/java/plugily/projects/minigamesbox/classic/commands/arguments/admin/LocaleArgument.java
@@ -0,0 +1,43 @@
+package plugily.projects.minigamesbox.classic.commands.arguments.admin;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import plugily.projects.minigamesbox.classic.commands.arguments.PluginArgumentsRegistry;
+import plugily.projects.minigamesbox.classic.commands.arguments.data.CommandArgument;
+import plugily.projects.minigamesbox.classic.commands.arguments.data.LabelData;
+import plugily.projects.minigamesbox.classic.commands.arguments.data.LabeledCommandArgument;
+import plugily.projects.minigamesbox.classic.handlers.language.MessageBuilder;
+import plugily.projects.minigamesbox.classic.utils.services.locale.Locale;
+
+public class LocaleArgument {
+
+  public LocaleArgument(PluginArgumentsRegistry registry) {
+    registry.mapArgument(registry.getPlugin().getCommandAdminPrefixLong(),
+        new LabeledCommandArgument("locale", registry.getPlugin().getCommandAdminPrefixLong() + ".admin.locale",
+            CommandArgument.ExecutorType.PLAYER,
+            new LabelData("/" + registry.getPlugin().getCommandAdminPrefix() + " locale &c[locale name/prefix]",
+                "/" + registry.getPlugin().getCommandAdminPrefixLong() + " locale &c[locale name/prefix]",
+                "&7Used for changing locale \n&6Permission: &7" + registry.getPlugin().getPluginNamePrefixLong() + ".admin.locale")) {
+          @Override
+          public void execute(CommandSender sender, String[] args) {
+            Player player = (Player) sender;
+            if (args.length != 2) {
+              new MessageBuilder(ChatColor.DARK_RED + "Please provide a locale name/prefix!").prefix().send(player);
+              return;
+            }
+            String localeName = args[1];
+            Locale locale = Locale.getLocale(localeName);
+            if (locale == null) {
+              new MessageBuilder(ChatColor.DARK_RED + "Locale not found!").prefix().send(player);
+              return;
+            }
+            registry.getPlugin().getConfig().set("locale", locale.getPrefix());
+            registry.getPlugin().saveConfig();
+
+            registry.getPlugin().getLanguageManager().setupLocale();
+          }
+        });
+  }
+
+}
