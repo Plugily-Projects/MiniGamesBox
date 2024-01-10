982c1e393404848f29652e4758637fa03ae4c835
Tigerpanzer_02 <37453987+Tigerpanzer02@users.noreply.github.com>
2024-01-10 21:58:26 +0100

    Moved Plugin-Chat-Format to Chat.Format in config.yml

--- MiniGamesBox Classic/src/main/java/plugily/projects/minigamesbox/classic/preferences/ConfigOption.java
+++ MiniGamesBox Classic/src/main/java/plugily/projects/minigamesbox/classic/preferences/ConfigOption.java
@@ -47,7 +47,7 @@ public class ConfigOption {
     options.put("BLOCK_IN_GAME_ITEM_MOVE", new ConfigOption("Block.In-Game.Item-Move", true, true));
     options.put("DATABASE", new ConfigOption("Database", false, true));
     options.put("REWARDS", new ConfigOption("Rewards", false, true));
-    options.put("PLUGIN_CHAT_FORMAT", new ConfigOption("Plugin-Chat-Format", true, true));
+    options.put("PLUGIN_CHAT_FORMAT", new ConfigOption("Chat.Format", true, true));
     options.put("SEPARATE_ARENA_CHAT", new ConfigOption("Chat.Separate.Arena", true, true));
     options.put("SEPARATE_ARENA_SPECTATORS", new ConfigOption("Chat.Separate.Spectators", true, true));
     options.put("FIREWORK", new ConfigOption("Firework", true, true));
